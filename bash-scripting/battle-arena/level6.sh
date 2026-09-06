ba25d4ec510866e07fce4de2270625798392d22f
mahamedm1 <mahamed.mahamud1@gmail.com>
2026-09-06 19:07:50 +0100

    Update level6.sh

--- bash-scripting/battle-arena/level6.sh
+++ bash-scripting/battle-arena/level6.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+set -x
+
+file_name=$1
+
+if [[ $# -ne 0 && -f $file_name ]]; then
+    echo "The number of lines in this file is:" $(wc -l < $file_name)
+else
+    echo "No file provided"
+fi
 
