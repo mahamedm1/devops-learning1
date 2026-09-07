3b6485ce7a4aeca899782084f76b542ca510aa7e
mahamedm1 <mahamed.mahamud1@gmail.com>
2026-09-07 02:56:06 +0100

    Level8 solution
    Added a script to search for a word in all .log files.

--- bash-scripting/battle-arena/level8.sh
+++ bash-scripting/battle-arena/level8.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+# will need to use grep
+# in all files ending in .log so *.log
+word=$1
 
+if [ $# -eq 0 ]; then 
+    echo "no phrase entered" 
+    exit 1 
+fi
+
+
+grep -l "$word" *.log 
